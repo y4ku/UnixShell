ed4a8cca31f64099e139d901b67be39be453854d
Jakub Misterka <san.y4ku@gmail.com>
2011-09-25 22:53:14 -0500

    Got stats working and detecting ampersand, now need to work on wait4 background process checking

--- shell.cpp
+++ shell.cpp
@@ -16,26 +16,33 @@ typedef struct commandTime {
    long userTime;
    long systemTime;
    bool background;
+   bool end;
 };
 
 char ** vectorCommand(char *, char *);
+bool backgroundCheck(char *, char *);
 char ** getArray(vector<char*>);
 pid_t forkPipe(char ** commands, int std_in, int std_out, bool, bool);
 char * fileNameWrite(char *, int count);
 char * fileNameRead(char *, int count);
-vector<commandTime *> status; 
+vector<commandTime *> statuses; 
+void printStats();
+void waitLoop();
+void quit();
+struct rusage timings;
 
 
 int main ( void ){
 
-struct rusage timings;
 char input[1024];
 int result = 0;
 int count = 0;
 pid_t * pipePid;
 char * filename[2];
-//vector<commandTime *> status; 
 
+bool background;
+
+char * Amp = "&";
 char * Pipe = "|";
 char * Space = " ";
 char * FileIn = "<";
@@ -52,6 +59,10 @@ while(result > -1){
     cin.getline(input, 1024, '\n');
 
     int check = 0;
+
+    if(backgroundCheck(input, Amp)){
+        background = true;
+    }
 
     char ** pipedCommands = vectorCommand(input, Pipe);
     
@@ -70,14 +81,32 @@ while(result > -1){
     
     for(int i = 0; pipedCommands[i] != NULL; i++){
 
+        if(strcmp(pipedCommands[i], "stats") == 0){
+            printStats();
+            break;
+        }
+
+        if(strcmp(pipedCommands[i], "exit") == 0){
+            quit();
+            break;
+        }
+
         filename[0] = NULL;
         filename[1] = NULL;
 
-        /*char * printme;
+        //char * pcom;
         const char * source = pipedCommands[i];
 
-        strcpy(printme, source);
-        cout << "COPY: " << printme << endl;*/        
+        statuses.push_back(new commandTime);
+
+        //statuses.back()->command = new char[sizeof(pcom)];
+        statuses.back()->command = new char[sizeof(source)];        
+
+        strcpy(statuses.back()->command, source);
+
+        statuses.back()->background = false;
+
+        cout << "From Vector: " << statuses.back()->command << endl;
 
         if(filename[0] = fileNameWrite(pipedCommands[i], i)){
             fileDesc0 = open(filename[0], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
@@ -115,6 +144,11 @@ while(result > -1){
                 
         pipePid[i] = forkPipe(commands, std_in, std_out, fileWrite, fileRead);
 
+        //Setting the pid for the stats struct
+        statuses.back()->pid = pipePid[i];
+
+        cout << statuses.back()->pid << endl;
+
         if(fileWrite){
             close(std_out);
             fileWrite = false;
@@ -133,13 +167,14 @@ while(result > -1){
         delete commands;
     }
     
-    int moose = 0;
-    while(moose < count){
-        cout << wait4(pipePid[moose], NULL, NULL, &timings) << endl;
-        moose++;
-        cout << "System Time: " << timings.ru_stime.tv_sec << " " << timings.ru_stime.tv_usec << " || ";
-        cout << "User Time:  " << timings.ru_utime.tv_sec << " " << timings.ru_utime.tv_usec << endl;
-    }  
+    //if(!background)    
+        waitLoop();
+
+    if(background){
+        cout << "BACKGROUND" << endl;
+        background = false;
+    }
+
 
     delete pipedCommands;
 }
@@ -147,6 +182,18 @@ while(result > -1){
 return 0;
 
 }
+
+bool backgroundCheck(char * input, char * delimeter){ 
+    char * amp;
+    if(amp = strchr(input, '&')){
+        *amp = NULL;
+        return true;
+    }
+    else 
+        return false;
+}
+
+
 char ** vectorCommand(char * input, char * delimeter){
     char * token;
 	vector<char*> charVector;
@@ -227,6 +274,38 @@ char * fileNameRead(char * pipeCommands, int count){
     }
     else return NULL;
 
+}
+
+void printStats(){
+    for(int i = 0; i < statuses.size(); i++){
+        cout << "Command: " <<  statuses.at(i)->command << " PID: " << statuses.at(i)->pid << endl;
+        cout << "User Time: " << statuses.at(i)->userTime << " || ";
+        cout << "System Time : " << statuses.at(i)->systemTime << endl;
+      }     
+}
+
+void waitLoop(){
+    for(int i = 0; i < statuses.size(); i++){
+        if(statuses.at(i)->background == false && statuses.at(i)->end == false){
+            wait4(statuses.at(i)->pid, NULL, NULL, &timings);
+            //Setting userTime and systemTime to stats struct
+            statuses.at(i)->userTime = timings.ru_stime.tv_usec;
+            statuses.at(i)->systemTime = timings.ru_utime.tv_usec;
+            statuses.at(i)->end = true;
+        }
+    }
+
+}
+
+void quit(){
+    //Garbage Collection
+    for(int i = 0; i < statuses.size(); i++){
+        delete statuses.at(i)->command; 
+    }
+    for(int i = 0; i < statuses.size(); i++){
+        delete statuses.at(i); 
+    }
+    exit(0);
 }
 
 
